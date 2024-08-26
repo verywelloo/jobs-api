888559277558b672ce17f0be3f0246609d8d1f46
verywelloo <verywelloo@hotmail.com>
2024-08-26 16:58:49 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+require("express-async-errors");
+
+// cookie
+// -in frontend, don't doing much more
+// -more secure in httpOnly
+// -downside, it is a little size
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const connectDB = require("./db/connect");
+
+const userRouter = require("./routes/userRoutes");
+const authRouter = require("./routes/authRoutes");
+const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const orderRouter = require("./routes/orderRoutes");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy");
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(helmet()); //security for header
+app.use(cors()); // let others domain access, for cookies
+app.use(xss()); // security for input
+app.use(mongoSanitize()); // security for mongo
+
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static("./public"));
+app.use(fileUpload());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
